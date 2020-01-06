9737fa762a41cb0d30ff559e67a7ff1028644156
Bysani <bchaithanya@opteamix.com>
2020-01-06 19:34:17 +0530

    First Commit

---
+++ XUnitTestProject/AuthorizationTest.cs
@@ -0,0 +1,20 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Xunit;
+
+
+namespace XUnitTestProject
+{
+    public class AuthorizationTest
+    {
+        [Fact]
+        public void IsAuthorizedToRunTest()
+        {
+            List<string> permissions = new List<string>();
+            permissions.Add("CanRunTest");
+            Assert.True(permissions.Contains("CanRunTest"),"permission is avaialble");
+        }
+    }
+}
